e0118ef8874a61382f811eed98eb3c906ce0445c
sunny kyung <shkyung@lunit.io>
2020-07-27 06:00:21 +0900

    Route in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import LoginScreen from './LoginScreen';
+import Admin from './Admin';
+import { AuthProvider } from './auth';
 
 function App() {
-  return <div className="App"></div>;
+  console.error("App.tsx called!! localStorage.getItem('accessToken')", localStorage.getItem('accessToken'));
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        {localStorage.getItem('accessToken') ? <Redirect to="/" /> : <Redirect to="login" />}
+        <Route exact path="/" render={() => <Admin />} />
+        <Route exact path="/login" render={(props) => <LoginScreen {...props} />} />
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
 export default App;
